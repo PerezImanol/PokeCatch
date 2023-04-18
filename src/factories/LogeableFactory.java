025e5019d2a7527c60405106f7dfc72ebcefd2f7
Alexander Epelde <alexander.epelde@gmail.com>
2023-04-18 08:18:58 +0200

    All interfaces implemented except simulable

--- src/factories/LogeableFactory.java
+++ src/factories/LogeableFactory.java
@@ -2,4 +2,6 @@ package factories;
 
 public class LogeableFactory {
 
+    
+
 }
--- src/interfaces/DataBattleShowable.java
+++ src/interfaces/DataBattleShowable.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+import java.util.LinkedHashSet;
+
+import classes.Combat;
+import classes.Pokemon;
+
 public interface DataBattleShowable {
 
+    public LinkedHashSet<Pokemon> getPokemons();
+    public LinkedHashSet<Combat> getTrainerCombatHistory(Integer trainerID);
 }
