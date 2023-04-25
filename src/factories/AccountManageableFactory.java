a02be3cc408e9af5f1a1e65ee9fbadf44019954a
Alexander Epelde <alexander.epelde@gmail.com>
2023-04-25 10:37:07 +0200

    Factories done

--- src/factories/AccountManageableFactory.java
+++ src/factories/AccountManageableFactory.java
@@ -1,5 +1,13 @@
 package factories;
 
+import controller.AccountManageableDBimplementation;
+import interfaces.AccountManageable;
+
 public class AccountManageableFactory {
+    private static AccountManageable obj = new AccountManageableDBimplementation();
+
+    public static AccountManageable getAccountManageable() {
+        return obj;
+    }
 
 }
